4888f009b89482ec09f9ae1ac6fa10b9eec8215a
Ashwini kale <ashukale1608@gmail.com>
2024-07-27 11:59:32 +0530

    create program api

---
+++ src/main/java/com/simplifyvms/config/repository/ProgramDao.java
@@ -0,0 +1,10 @@
+package com.simplifyvms.config.repository;
+
+import com.simplifyvms.config.entity.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface ProgramDao extends JpaRepository<Program, UUID> {
+}
